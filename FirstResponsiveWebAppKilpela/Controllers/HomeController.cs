5cad109b3aa16551e20f5d02f6d04299f08ea013
Donovan Kilpela <161644962+DonovanKilpela@users.noreply.github.com>
2025-01-19 12:59:15 -0600

    FRWAK
    Adding the completed assignment for Responsive Web App.

---
+++ FirstResponsiveWebAppKilpela/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using FirstResponsiveWebAppKilpela.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstResponsiveWebAppKilpela.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AgeData ageData)
+        {
+            // Once form is completed and submitted it will show this message out to the user on the bottom of the page
+            if (ModelState.IsValid)
+            {
+                ViewBag.Result = $"{ageData.Name}, will be {ageData.AgeThisYear()} years old on December 31st, " +
+                    $"{Constants.CURRENT_YEAR}.";
+                return View(ageData);
+            }
+            return View(ageData);
+        }
+    }
+}
---
+++ FirstResponsiveWebAppKilpela/Models/AgeData.cs
@@ -0,0 +1,29 @@
+﻿// Adding this so I can make sure that data validation takes place 
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppKilpela.Models
+{
+    // Creating of the global constant 
+    static class Constants
+    {
+        public const int CURRENT_YEAR = 2025;
+    }
+
+    public class AgeData
+    {
+        // Variable declaration 
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Birth year is required")]
+        [Range(1925, 2025, ErrorMessage = "Birth Year must be between 1925 - 2025")]
+        public int birthYear { get; set; }
+
+        // Method to get the age of the user at the end of the year 
+        public int AgeThisYear()
+        {
+            return Constants.CURRENT_YEAR - birthYear;
+        }
+
+    }
+}
